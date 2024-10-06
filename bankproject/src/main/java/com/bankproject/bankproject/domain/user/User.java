750a05c09558b1d441a382851d24dca45b3fb7d5
Mindundun <pmk10077@gmail.com>
2024-10-06 21:18:23 +0900

    UserEntityModify

--- bankproject/src/main/java/com/bankproject/bankproject/domain/user/User.java
+++ bankproject/src/main/java/com/bankproject/bankproject/domain/user/User.java
@@ -1,42 +1,40 @@
 package com.bankproject.bankproject.domain.user;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
     
-    public User(){
-
+    public User() {
     }
 
     @Id 
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID 생성 전략 추가
     private Long id;
-    // private String password;
 
     private String username;
     
-    public long getId(){
+    public Long getId() { // Long 타입으로 반환
         return id;
     }
 
-    public void setId(long id){
-        this.id =id;
+    public void setId(Long id) { // Long 타입으로 매개변수
+        this.id = id;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username){
-        this.username =username;
+    public void setUsername(String username) {
+        this.username = username;
     }
     
     @Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
-	}
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + "]";
+    }
 }
