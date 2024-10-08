74f84d6a674851bc9f3210b989ae4566de776662
hjh <hjh_0524@naver.com>
2024-10-08 23:57:14 +0900

    test.html 생성

--- bankproject/src/main/resources/static/js/test.js
+++ bankproject/src/main/resources/static/js/test.js
@@ -1,3 +1,11 @@
-window.onload = function() {
-    alert("Hello World!");
-};
+/* window.onload = function(){
+    let navList = document.querySelectorAll(".main-menu");
+    
+    navList.forEach(navItem => {
+        navItem.addEventListener("mouseover", function(){
+            this.querySelector(".sub-nav").style.height = '155px'
+        });
+    });
+}
+
+ */
