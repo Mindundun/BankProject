5ee1b1e8715c52dacf674dce451dd9028a91f611
kimc <tjdtlr12349@gmail.com>
2024-10-12 20:13:48 +0900

    임시 게시판 파일

--- bankproject/src/main/java/com/bankproject/bankproject/domain/board/entity/Board.java
+++ bankproject/src/main/java/com/bankproject/bankproject/domain/board/entity/Board.java
@@ -1,11 +1,15 @@
 package com.bankproject.bankproject.domain.board.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.bankproject.bankproject.domain.board.dto.FileDTO;
 import com.bankproject.bankproject.domain.board.enums.BoardType;
+import com.bankproject.bankproject.entity.UserEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,7 +19,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,15 +47,19 @@ public class Board {
     @Enumerated(EnumType.STRING)
     private BoardType type;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
     private String title;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    private String author;
+    @Column(columnDefinition = "JSON")
+    private String files;
 
     @Column(name = "created_date")
-    @CreatedDate
     private LocalDateTime createdDate;
 
     @Column(name = "read_count")
@@ -63,8 +74,19 @@ public class Board {
             this.readCount = 0;
         }
 
+        if(this.createdDate == null) {
+            this.createdDate = LocalDateTime.now();
+        }
+
         if(this.isDeleted == null) {
             this.isDeleted = false;
+        }
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        if(this.createdDate == null) {
+            this.createdDate = LocalDateTime.now();
         }
     }
 
@@ -74,6 +96,24 @@ public class Board {
 
     public void delete() {
         this.isDeleted = true;
+    }
+
+    public void setFiles(List<FileDTO> fileDTOList) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            this.files = objectMapper.writeValueAsString(fileDTOList);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("파일 정보를 JSON으로 변환하는데 실패했습니다.");
+        }
+    }
+
+    public List<FileDTO> getFiles() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(this.files, objectMapper.getTypeFactory().constructCollectionType(List.class, FileDTO.class));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("파일 정보를 객체로 변환하는데 실패했습니다.");
+        }
     }
 
 }
